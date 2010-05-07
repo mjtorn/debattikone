5bf0d1474c821ea9eaee7fbea8d741c2bad3b067
Markus Törnqvist <mjt@fadconsulting.com>
2010-05-07 15:44:06 +0300

    Insane customs

---
+++ custom_settings.py
@@ -0,0 +1,15 @@
+# vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Markus Törnqvist', 'mjt@fadconsulting.com',
+)
+
+TIME_ZONE = 'Europe/Helsinki'
+
+DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+
+# EOF
+
