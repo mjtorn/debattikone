2f96a5533f2386a4664f770461aa91760b0d2b2e
Markus Törnqvist <mjt@fadconsulting.com>
2010-05-07 17:09:05 +0300

    Flesh out models

--- debattikone/web/models.py
+++ debattikone/web/models.py
@@ -2,7 +2,24 @@
 
 from django.db import models
 
+from django.contrib.auth import models as auth_models
+
 # Create your models here.
+
+class Topic(models.Model):
+    title = models.CharField(max_length=64)
+    summary = models.CharField(max_length=1025)
+
+
+class DebateMessage(models.Model):
+    user = models.ForeignKey(auth_models.User)
+    topic = models.ForeignKey(Topic)
+
+
+class Debate(models.Model):
+    topic = models.ForeignKey(Topic)
+    user1 = models.ForeignKey(auth_models.User)
+    user2 = models.ForeignKey(auth_models.User, null=True, blank=True, default=None)
 
 # EOF
 
