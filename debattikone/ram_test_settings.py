a1bcee262c4b3f683e5954cd1b7cd4cf81a8e490
Markus Törnqvist <mjt@fadconsulting.com>
2010-05-08 20:10:55 +0300

    Execute tests in ram

---
+++ debattikone/ram_test_settings.py
@@ -0,0 +1,5 @@
+from settings import *
+
+DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = ':memory:'             # Or path to database file if using sqlite3.
+
