bd1a39ca04139a1e3dbb97da00ca4bf1c4b0ec69
Markus Törnqvist <mjt@fadconsulting.com>
2010-05-10 22:29:33 +0300

    index

--- debattikone/web/views.py
+++ debattikone/web/views.py
@@ -1,8 +1,16 @@
 # vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
 
+from annoying.decorators import render_to
+from django.template import RequestContext
+
 # Create your views here.
 
-
+@render_to('index.html')
+def index(request):
+    context = {
+        'title': 'Etusivu',
+    }
+    return context
 
 # EOF
 
