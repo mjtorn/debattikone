3b1efb899c7b1db6cb019b083a3bbfa23219e31a
Markus Törnqvist <mjt@fadconsulting.com>
2010-05-10 12:46:46 +0300

    While at it, modernise ram_test_settings

--- debattikone/ram_test_settings.py
+++ debattikone/ram_test_settings.py
@@ -1,5 +1,5 @@
 from settings import *
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ':memory:'             # Or path to database file if using sqlite3.
+DATABASES['default']['ENGINE'] = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASES['default']['NAME'] = ':memory:'             # Or path to database file if using sqlite3.
 
