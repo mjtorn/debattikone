079f4caf80eeb296c47e311ba4fb9e2ad5200439
Markus Törnqvist <mjt@fadconsulting.com>
2010-05-12 12:22:09 +0300

    Test ajax to follow debate

--- debattikone/web/test_030_web.py
+++ debattikone/web/test_030_web.py
@@ -2,16 +2,23 @@
 
 from django.core.management import call_command
 
+from django.core.urlresolvers import reverse
+
 from django.db import connections
 
+from django.test import Client
+
 fixtures = ['test_data.json']
+
+c = Client()
 
 def setup():
     for db in connections:
         if globals().has_key('fixtures'):
             call_command('loaddata', *globals()['fixtures'], **{'verbosity': 0, 'commit': False, 'database': db})
 
-
+def test_010_test_follow():
+    c.get(reverse('follow_debate', args=('1', 'x')))
 
 def teardown():
     for db in connections:
