6e2cfbafe29a948e282e50c180aefde6bb82f7f1
Markus Törnqvist <mjt@fadconsulting.com>
2010-05-20 19:18:08 +0300

    Can participate?

---
+++ debattikone/web/templatetags/debattikone_filters.py
@@ -0,0 +1,12 @@
+# vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
+
+from django.template import Library
+
+register = Library()
+
+@register.filter
+def can_participate(debate, user):
+    return debate.can_participate(user)
+
+# EOF
+
